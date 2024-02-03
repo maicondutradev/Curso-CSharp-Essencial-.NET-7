fd401c03f61f2e25a59fcda2f9a91367e0972de7
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-03 10:05:06 -0300

    CSharp

--- ClassesObjetos/Program.cs
+++ ClassesObjetos/Program.cs
@@ -1,8 +1,35 @@
-﻿namespace ClassesObjetos;
-class Program
+﻿namespace ClassesObjetos
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Pessoas();
+        }
+
+        static void Pessoas()
+        {
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Maicon";
+            p1.idade = 23;
+            p1.sexo = "Masculino";
+            Console.WriteLine($"{p1.nome} {p1.idade} {p1.sexo}");
+
+
+            Pessoa p2 = new Pessoa();
+            p2.nome = "Mirela";
+            p2.idade = 22;
+            p2.sexo = "Feminino";
+            Console.WriteLine($"{p2.nome} {p2.idade} {p2.sexo}");
+        }
+
+    }
+
+    class Pessoa
+    {
+        public string? nome;
+        public int idade;
+        public string? sexo;
+
     }
 }
