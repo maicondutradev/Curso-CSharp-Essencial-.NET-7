9cf0c187031fd62b5c0eaa27f3f55c92b535fe29
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-06 14:05:34 -0300

    CSharp

--- Exercicios/ExerciciosPraticos2/Program.cs
+++ Exercicios/ExerciciosPraticos2/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
+            Carro carro = new Carro("Sedan", "Chevrolet");
+            Console.WriteLine($"O carro do modelo {carro.Modelo} e da montadora {carro.Montadora}");
 
+
+
+        }
+    }
+
+    public class Carro
+    {
+
+        public string Modelo;
+        public string Montadora;
+
+        public Carro(string modelo, string montadora)
+        {
+            Modelo = modelo;
+            Montadora = montadora;
+
+            /*
+                Comportamento do Construtor:
+                O construtor da classe Carro é utilizado para criar objetos do tipo Carro quando a linha Carro carro = new Carro("Sedan", "Chevrolet"); é executada no método Main.
+                Este construtor atribui os valores dos parâmetros modelo e montadora às propriedades correspondentes (Modelo e Montadora) da instância recém-criada.
+            */
+        }
+
+
+        public double VelocidadeMaximo(int potencia)
+        {
+            return potencia * 1.75;
+
+            /*
+                Comportamento da Palavra-Chave return:
+                A palavra-chave return é usada no método VelocidadeMaximo.
+                Esse método recebe um parâmetro potencia e calcula a velocidade máxima com base nessa potência multiplicada por 1.75.
+                O valor calculado é, então, retornado pelo método usando a palavra-chave return.
+                No contexto de um método, return é usado para devolver um valor e encerrar a execução do método. Neste caso, o resultado do cálculo é retornado à chamada do método.
+            */
         }
     }
 
