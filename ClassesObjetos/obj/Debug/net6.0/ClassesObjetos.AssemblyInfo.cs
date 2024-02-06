e2b138d0e2d9ee895aaab97abc761e9d7d071561
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-06 15:10:20 -0300

    CSharp

--- ClassesObjetos/obj/Debug/net6.0/ClassesObjetos.AssemblyInfo.cs
+++ ClassesObjetos/obj/Debug/net6.0/ClassesObjetos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassesObjetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf6327b203408b80338f575ab57e799febbe439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf0c187031fd62b5c0eaa27f3f55c92b535fe29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassesObjetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassesObjetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
