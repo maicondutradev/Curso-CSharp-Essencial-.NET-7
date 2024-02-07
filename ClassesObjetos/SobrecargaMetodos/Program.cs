c0f45e5bb51c30cad7e00b17c612440122d8acc0
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-06 23:30:04 -0300

    CSharp

---
+++ ClassesObjetos/SobrecargaMetodos/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Email
+{
+    public void Enviar(string endereco)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine("Assunto padrão");
+    }
+
+    public void Enviar(string endereco, string assunto)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"{assunto}");
+    }
+
+    public void Enviar(string endereco, decimal valor)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine("Proposta comercial");
+        Console.WriteLine(valor);
+    }
+
+    public void Enviar(decimal valor, string endereco)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine("Pagamento fornecedor");
+        Console.WriteLine(valor);
+    }
+}
