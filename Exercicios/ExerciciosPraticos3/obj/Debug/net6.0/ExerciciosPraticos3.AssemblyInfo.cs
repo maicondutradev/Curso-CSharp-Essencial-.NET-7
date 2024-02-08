cf644e00a6890393f9fec3c6f5e01ee8f3708471
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-08 15:00:38 -0300

    CSharp

--- Exercicios/ExerciciosPraticos3/obj/Debug/net6.0/ExerciciosPraticos3.AssemblyInfo.cs
+++ Exercicios/ExerciciosPraticos3/obj/Debug/net6.0/ExerciciosPraticos3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExerciciosPraticos3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61c3297595c8f672fea9e17cc9c9498ed8cdb12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0bfa828129d9eb933052ca8a108540b13f3fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExerciciosPraticos3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExerciciosPraticos3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
