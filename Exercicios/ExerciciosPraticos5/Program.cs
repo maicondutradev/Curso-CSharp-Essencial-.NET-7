ac78385eaea49db910646c441235b6f205649480
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-09 11:14:57 -0300

    CSharp

---
+++ Exercicios/ExerciciosPraticos5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExerciciosPraticos5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Informacao informacao = new Informacao();
+
+            informacao.ExibirInfo(modelo: "Sedan", montadora: "Chevrolet", marca: "Onix", potencia: 110);
+            Console.WriteLine(" ");
+            informacao.ExibirInfo(modelo: "Sedan", montadora: "Chevrolet", marca: "Onix", potencia: 110, ano: 2016);
+        }
+    }
+
+    public class Informacao
+    {
+        public void ExibirInfo(string modelo, string montadora, string marca, int potencia, int ano = 2010)
+        {
+            Console.WriteLine($"Modelo: {modelo} \n Montadora: {montadora} \n Marca: {marca} \n Potência: {potencia} \n Ano: {ano} ");
+        }
+    }
+}
