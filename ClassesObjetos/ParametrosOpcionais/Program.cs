acc8df158e72d27664aac51fded29c08d0b58f4d
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-09 10:56:16 -0300

    CSharp

--- ClassesObjetos/ParametrosOpcionais/Program.cs
+++ ClassesObjetos/ParametrosOpcionais/Program.cs
@@ -4,15 +4,28 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Informe o endereço: ");
+            var destino = Console.ReadLine();
+
+            Console.WriteLine("Informe o titulo: ");
+            var titulo = Console.ReadLine();
+
+            Console.WriteLine("Informe o assunto: ");
+            var assunto = Console.ReadLine();
+
+
             Email email = new Email();
-            email.Enviar("teste@gmail.com");
+            email.Enviar();
+            email.Enviar(destino);
+            email.Enviar(destino, titulo);
+            email.Enviar(destino, titulo, assunto);
 
         }
     }
 
     public class Email
     {
-        public void Enviar(string destino, string titulo = "Orçamento", string assunto = "Aprovação")
+        public void Enviar(string destino = "teste@gmail.com", string titulo = "Orçamento", string assunto = "Aprovação")
         {
             Console.WriteLine($"\n Para {destino} - {titulo} \n Assunto: {assunto}");
         }
