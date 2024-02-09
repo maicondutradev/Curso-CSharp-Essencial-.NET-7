2436869d6120fbea58266a525c9852077b202d6b
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-09 13:26:50 -0300

    CSharp

---
+++ ClassesObjetos/MetodosEstaticos/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        var soma = Calculadora.Somar(5, 10);
+        Console.WriteLine(soma);
+    }
+    public class Calculadora
+    {
+        public static int Somar(int n1, int n2)
+        {
+            Console.WriteLine($"Operação de adição dos valores {n1} + {n2} = {n1 + n2}");
+            return n1 + n2;
+        }
+
+        public static int Subtrair(int n1, int n2)
+        {
+            Console.WriteLine($"Operação de subtração dos valores {n1} - {n2} = {n1 - n2}");
+            return n1 - n2;
+        }
+
+        public static int Multiplicar(int n1, int n2)
+        {
+            Console.WriteLine($"Operação de Multiplicação dos valores {n1} * {n2} = {n1 * n2}");
+            return n1 * n2;
+        }
+
+        public static int Dividir(int n1, int n2)
+        {
+            Console.WriteLine($"Operação de divisão dos valores {n1} / {n2} = {n1 / n2}");
+            return n1 / n2;
+        }
+    }
+}
