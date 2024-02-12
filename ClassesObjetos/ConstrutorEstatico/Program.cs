53f43d0e5adb4158a844435a19ebeb8997ae88bf
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-12 08:31:46 -0300

    CSharp

---
+++ ClassesObjetos/ConstrutorEstatico/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConstrutorEstatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa(23, "Maicon");
+            Console.WriteLine($"{p1.Nome} - {p1.Idade}");
+            Console.WriteLine($"Idade minima: {Pessoa.IdadeMinima}");
+
+            Pessoa p2 = new Pessoa(22, "Mirela");
+            Console.WriteLine($"{p2.Nome} - {p2.Idade}");
+            Console.WriteLine($"Idade minima: {Pessoa.IdadeMinima}");
+        }
+    }
+
+
+}
