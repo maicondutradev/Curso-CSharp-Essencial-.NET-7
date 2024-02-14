83a9e67d84361c494536b9d23fdc056de84a19d2
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-13 22:12:16 -0300

    Partil Class

---
+++ ClassesObjetos/PartialClass/MinhaPartialClassProps.cs
@@ -0,0 +1,7 @@
+
+
+public partial class MinhaPartialClass
+{
+    public DateTime DataNascimeto { get; set; }
+    public string? Nome { get; set; }
+}
---
+++ ClassesObjetos/PartialClass/Program.cs
@@ -0,0 +1,13 @@
+﻿MinhaPartialClass minhaPartialClass = new MinhaPartialClass();
+
+Console.WriteLine("Idade para nascidos em: 20/04/2000\n");
+
+var idade = minhaPartialClass.CalculaIdade(new DateTime(2000, 4, 20));
+Console.WriteLine($"{Math.Round(idade.TotalDays / 365)} anos");
+
+Console.WriteLine("\n Idade para nascidos em: 17/07/2001\n");
+
+var data1 = DateTime.Now;
+var data2 = new DateTime(2001, 07, 17);
+var diferenca = minhaPartialClass.DiferencaEntreDatas(data1, data2);
+Console.WriteLine($"{Math.Round(diferenca.TotalDays / 365)} anos");
