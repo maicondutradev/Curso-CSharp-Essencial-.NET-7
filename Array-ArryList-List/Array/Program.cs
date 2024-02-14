aedfc9bcfc6d40c027367cde515b188b96335dd9
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-14 09:00:06 -0300

    docs:Array

---
+++ Array-ArryList-List/Array/Program.cs
@@ -0,0 +1,9 @@
+﻿//Array de números inteiros simples.
+int[] numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Console.WriteLine(numeros[2]);
+
+//Array de strings, três maneiras de representar esse array.
+string[] nome1 = new string[5] { "Maicon", "Mirela", "Izabel", "Mario", "Naruto" };
+string[] nome2 = new string[] { "Maicon", "Mirela", "Izabel", "Mario", "Naruto" };
+string[] nome3 = { "Maicon", "Mirela", "Izabel", "Mario", "Naruto" };
+Console.WriteLine(nome3[1]);
