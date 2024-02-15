76844833be49d73771e2a067cb6e413bfa1981b8
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-15 08:19:27 -0300

    docs:Array3

---
+++ Array-ArryList-List/Array3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayNomes();
+        }
+        static void ArrayNumeros()
+        {
+            int[] numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+        static void ArrayNomes()
+        {
+            string[] nomes = new string[] { "Maicon", "Mirela", "Izabel", "Naruto", "Yuta" };
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
