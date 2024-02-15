de8548cb41bea10503efd046d1080c90cbbbc3fc
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-15 10:52:53 -0300

    docs:Modificador Params

---
+++ Array-ArryList-List/ModificadorParams/Program.cs
@@ -0,0 +1,16 @@
+﻿var resultado = Calcular.Soma(1, 2, 3, 4, 5);
+
+Console.WriteLine(resultado);
+
+public class Calcular
+{
+    public static int Soma(params int[] numeros)
+    {
+        int total = 0;
+        foreach (var numero in numeros)
+        {
+            total += numero;
+        }
+        return total;
+    }
+}
