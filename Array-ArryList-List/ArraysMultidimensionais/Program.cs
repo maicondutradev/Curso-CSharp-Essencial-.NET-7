9c32bd33a293899db039c19bea2b14d0cb136f42
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-16 08:09:59 -0300

    CSharp

---
+++ Array-ArryList-List/ArraysMultidimensionais/Program.cs
@@ -0,0 +1,8 @@
+﻿//Exemplo de um array Bidimensional
+//int[,] numeros = { { 1, 2 }, { 3, 4 } };
+int[,] numeros = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+
+Console.WriteLine(numeros[0, 0]);
+Console.WriteLine(numeros[0, 1]);
+Console.WriteLine(numeros[1, 0]);
+Console.WriteLine(numeros[1, 1]);
--- Array-ArryList-List/obj/Debug/net7.0/Array-ArryList-List.AssemblyInfo.cs
+++ Array-ArryList-List/obj/Debug/net7.0/Array-ArryList-List.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a9e67d84361c494536b9d23fdc056de84a19d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8548cb41bea10503efd046d1080c90cbbbc3fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
