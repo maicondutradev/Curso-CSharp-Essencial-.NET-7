a6fe14ad1863040e4d14c485dd8801dd1b81b5f6
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-16 08:25:54 -0300

    CSharp

--- Array-ArryList-List/ArrayMultidimensional2/Program.cs
+++ Array-ArryList-List/ArrayMultidimensional2/Program.cs
@@ -2,23 +2,22 @@
 {
     static void Main(string[] args)
     {
+        // Criando uma matriz bidimensional (array) de inteiros com dimensões 3x3
         int[,] n = new int[3, 3] {
-            { 11, 22, 33 }, //n[0,0] n[0,1] n[0,2]
-            { 44, 55, 66 }, //n[1,0] n[1,1] n[1,2]    n[3,3] -> n[i,j]
-            { 77, 88, 99 }  //n[2,0] n[2,1] n[2,2]
-            };
+            { 11, 22, 33 }, // Linha 0: n[0,0], n[0,1], n[0,2]
+            { 44, 55, 66 }, // Linha 1: n[1,0], n[1,1], n[1,2]
+            { 77, 88, 99 }  // Linha 2: n[2,0], n[2,1], n[2,2]
+        };
 
+        // Loop externo para percorrer as linhas da matriz
         for (int i = 0; i < n.GetLength(0); i++)
         {
+            // Loop interno para percorrer as colunas da matriz
             for (int j = 0; j < n.GetLength(1); j++)
             {
+                // Imprime o valor do elemento atual da matriz
                 Console.WriteLine($"{n[i, j]}");
             }
-        }
-
-        foreach (int x in n)
-        {
-            Console.WriteLine($"{x}");
         }
     }
 }
--- Array-ArryList-List/obj/Debug/net7.0/Array-ArryList-List.AssemblyInfo.cs
+++ Array-ArryList-List/obj/Debug/net7.0/Array-ArryList-List.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8548cb41bea10503efd046d1080c90cbbbc3fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c32bd33a293899db039c19bea2b14d0cb136f42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
