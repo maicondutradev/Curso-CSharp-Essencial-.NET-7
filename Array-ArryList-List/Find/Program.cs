9fad49c7eba517c26aed14a9d6b76ec25816dcdd
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-19 15:20:45 -0300

    Find()

---
+++ Array-ArryList-List/Find/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> frutas = new List<string>()
+{
+    "Uva","Banana","Maça","Abacate","Laranja","Morango"
+};
+
+//Usando o Predicado como uma função
+var fruta1 = frutas.Find(Procura);
+Console.WriteLine($"Usando Predicado - {fruta1}");
+
+//Usando a expressão lambda
+var fruta2 = frutas.Find(f => f.Contains('n'));
+Console.WriteLine($"Expressão Lambda - {fruta2}");
+
+//Usando o Predicado como uma função
+static bool Procura(string item)
+{
+    return item.Contains('n');
+
+    //return item.StartsWith('L');
+}
+
--- Array-ArryList-List/obj/Debug/net7.0/Array-ArryList-List.AssemblyInfo.cs
+++ Array-ArryList-List/obj/Debug/net7.0/Array-ArryList-List.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72add229216026e42d8a356c43db3d82bcd6e69d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad599c9cbe9f146ca7bc45b985e0ae2f4e504007")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array-ArryList-List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
