f3148e2acf062830e228092ba7bc96a9b0716441
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-25 22:37:28 -0300

    docs: Herança Modificadores de Acesso

---
+++ OrientadoAObjetos/Heranca_ModifAcesso/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using Microsoft.VisualBasic;
+
+ClasseDerivada cd = new ClasseDerivada();
+cd.VerificaAcesso();
+
+public class ClasseDerivada : ClasseBase
+{
+    public void VerificaAcesso()
+    {
+        Public_Membro();
+
+        Protected_Membro();
+
+        Internal_Membro();
+
+        Protected_Internal_Membro();
+
+        //Private_Membro();
+
+        Console.WriteLine("\nAcesso ao campo private : private_var usando uma propriedade GET");
+
+        int soma = public_var + protected_var + internal_var + Private_var;
+        Console.WriteLine($"\nTotal soma : {soma}");
+
+    }
+}
+
+public class ClasseBase
+{
+    //Campos
+    public int public_var = 1;
+    protected int protected_var = 2;
+    internal int internal_var = 3;
+    private int private_var = 4;
+
+    //Propriedade
+    public int Private_var
+    {
+        get { return private_var; }
+        set { private_var = value; }
+    }
+
+    public void Public_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Public");
+    }
+    protected void Protected_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Protected");
+    }
+    internal void Internal_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Internal");
+    }
+    protected internal void Protected_Internal_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Protected Internal");
+    }
+    private void Private_Membro()
+    {
+        Console.WriteLine("ClasseBase - Método Private");
+    }
+
+}
