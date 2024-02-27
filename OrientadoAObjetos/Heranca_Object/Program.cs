9260b43917149003cbc25b0eef59b0a8ae39de7f
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-27 13:53:49 -0300

    docs: Herança Object

---
+++ OrientadoAObjetos/Heranca_Object/Program.cs
@@ -0,0 +1,23 @@
+﻿public struct Struct1
+{
+
+}
+
+public class Classe1
+{
+
+}
+public class Classe2
+{
+
+}
+//Uma classe não pode herdar mais de 1 classe
+public class ClasseDerivada : Classe1, Classe2
+{
+
+}
+//Uma classe não pode herdar uma struct
+public class ClasseDerivada2 : Struct1
+{
+
+}
