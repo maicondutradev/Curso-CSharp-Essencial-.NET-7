3db330c8407afce68d7807ec8a1ce8b6ea1112c4
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-02-29 23:34:06 -0300

    learn csharp

---
+++ OrientadoAObjetos/Heranca_ModifNew/Program.cs
@@ -0,0 +1,32 @@
+﻿// Cria uma instância da classe Aluno chamada 'aluno'
+Aluno aluno = new Aluno();
+
+// Define o atributo 'Nome' da instância 'aluno' como "Maicon"
+aluno.Nome = "Maicon";
+
+// Define o atributo 'Curso' da instância 'aluno' como "Engenharia de Software"
+aluno.Curso = "Engenharia de Software";
+
+// Imprime a saudação da instância 'aluno' usando o método 'Saudacao' da classe 'Aluno'
+Console.WriteLine(aluno.Saudacao());
+// Classe base 'Pessoa'
+class Pessoa
+{
+    // Propriedade 'Nome' com getter e setter
+    public string? Nome { get; set; }
+
+    // Método que retorna uma saudação utilizando o nome da pessoa
+    public string Saudacao() => $"Oi, Eu sou o(a) {Nome}";
+}
+
+// Classe derivada 'Aluno' que herda de 'Pessoa'
+class Aluno : Pessoa
+{
+    // Propriedade 'Curso' com getter e setter
+    public string? Curso { get; set; }
+
+    // Método que retorna uma saudação específica para um aluno, incluindo o nome e o curso
+    // Aviso: Este método está ocultando o método Saudacao da classe base Pessoa
+    // É intencional e usa a palavra-chave 'new' para indicar isso.
+    public new string Saudacao() => $"Oi, Eu sou o(a) {Nome} do curso de : {Curso}";
+}
