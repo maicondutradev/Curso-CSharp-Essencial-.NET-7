3ead94aefe65275dab2cbc52f9d1d1ccdd8b8209
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-03-04 11:22:12 -0300

    docs: Herança Sealed

---
+++ OrientadoAObjetos/Heranca_Sealed/Program.cs
@@ -0,0 +1,45 @@
+﻿// Definição do namespace VirtualOverridd
+namespace VirtualOverridd
+{
+    // Classe principal do programa
+    class Program
+    {
+        // Método de entrada principal
+        static void Main(string[] args)
+        {
+            // Criando uma instância da classe Cliente
+            Cliente cliente = new Cliente();
+
+            // Atribuindo um nome à propriedade Nome da instância
+            cliente.Nome = "Maicon";
+
+            // Chamando o método ExibeNome da classe Cliente
+            cliente.ExibeNome();
+        }
+    }
+
+    // Classe Pessoa, marcada como sealed para impedir a herança
+    sealed class Pessoa
+    {
+        // Propriedade Nome que pode ser nula (?)
+        public string? Nome { get; set; }
+
+        // Método para exibir o nome
+        public void ExibeNome()
+        {
+            // Exibindo o nome formatado
+            Console.WriteLine($"\nMeu nome é {Nome}");
+        }
+    }
+
+    // Classe Cliente, herda da classe Pessoa, Porém não pode pois a classe pessoa possui SLEADED = Impede a herança
+    class Cliente : Pessoa
+    {
+        // Sobrescrevendo o método ExibeNome da classe base Pessoa usando "new"
+        public new void ExibeNome()
+        {
+            // Exibindo o nome do cliente formatado
+            Console.WriteLine($"\nNome do cliente: {Nome}");
+        }
+    }
+}
