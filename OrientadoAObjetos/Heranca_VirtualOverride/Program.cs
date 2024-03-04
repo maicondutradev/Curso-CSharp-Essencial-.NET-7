87ac38a06e16ebc2338c9dd389b4ca32e61df337
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-03-04 10:59:01 -0300

    docs: Herança Virtual Override

---
+++ OrientadoAObjetos/Heranca_VirtualOverride/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace VirtualOverridd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gato gato = new Gato();
+            gato.Nome = "Mel";
+            gato.ExibeNome();
+
+            Cachorro cachorro = new Cachorro();
+            cachorro.Nome = "Faisca";
+            cachorro.ExibeNome();
+
+            Console.ReadKey();
+        }
+    }
+    //Classe Base
+    class Animal
+    {
+        public string? Nome { get; set; }
+        public virtual void ExibeNome()
+        {
+            Console.WriteLine($"\nMeu nome é {Nome}");
+        }
+    }
+    //Classe derivada
+    class Gato : Animal
+    {
+        public override void ExibeNome()
+        {
+            Console.WriteLine($"\nEu sou um gato. Meu nome é : {Nome}");
+        }
+    }
+
+    class Cachorro : Animal
+    {
+
+    }
+
+}
