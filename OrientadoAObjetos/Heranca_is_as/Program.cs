df6fcd5a4c0e5fd96d73da2dffeff7641ed79e20
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-03-06 10:53:01 -0300

    docs: Herança is e as

---
+++ OrientadoAObjetos/Heranca_is_as/Program.cs
@@ -0,0 +1,15 @@
+﻿Object carroObj = new Carro();
+
+var x = carroObj as string;
+
+if (x != null)
+{
+    Console.WriteLine("A conversão funcionou -> obj para string!");
+}
+else
+{
+    Console.WriteLine("A conversão falhou, x é null...");
+}
+
+class Carro
+{ }
