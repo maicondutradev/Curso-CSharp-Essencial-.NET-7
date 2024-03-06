c5c5c6d0d8d7a7ce7814081eb6e4795915b733d5
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-03-06 10:18:17 -0300

    docs: Upcasting e Downcasting

---
+++ OrientadoAObjetos/Heranca_UpcastingDowncasting/Forma.cs
@@ -0,0 +1,17 @@
+namespace UpcastingDowncasting;
+
+public class Forma
+{
+    protected int xpos, ypos;
+    public Forma()
+    {}
+    public Forma(int x, int y)
+    {
+        xpos = x;
+        ypos = y;
+    }
+    public virtual void Desenhar()
+    {
+        Console.WriteLine($"Desenhar - Forma na posição : ({xpos},{ypos})");
+    }
+}
---
+++ OrientadoAObjetos/Heranca_UpcastingDowncasting/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UpcastingDowncasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circulo = new Circulo(10, 20);
+            Forma forma = circulo;//Upcasting -> implicita
+
+            Circulo c = (Circulo)forma;//Downcasting -> explicita
+
+            c.PintarCirculo();
+            c.Desenhar();
+
+            Console.WriteLine(c == forma);
+            Console.WriteLine(c == circulo);
+
+        }
+    }
+}
