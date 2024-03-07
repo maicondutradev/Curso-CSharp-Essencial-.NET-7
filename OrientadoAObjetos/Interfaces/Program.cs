4bf120977d6593bd3423e59168ad52effe03147e
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-03-07 15:37:50 -0300

    docs: Interfaces

---
+++ OrientadoAObjetos/Interfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+
+namespace Interfaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IControle demo = new Demo();
+        demo.Exibir();
+        demo.Desenhar();
+        demo.Nome = "Maicon";
+
+        IGrafico demo_ = new Demo();
+        demo_.Pintar();
+
+
+        Console.ReadKey();
+    }
+    interface IControle
+    {
+        string Nome { get; set; }
+        void Desenhar();
+        public void Exibir()
+        {
+            Console.WriteLine("teste...");
+        }
+    }
+    interface IGrafico
+    {
+        void Pintar();
+    }
+    public class Demo : IControle, IGrafico
+    {
+        public string Nome { get; set; } = string.Empty;
+
+        public void Desenhar()
+        {
+            Console.WriteLine("Desenhando...");
+        }
+
+        public void Pintar()
+        {
+            Console.WriteLine("Pintando...");
+        }
+    }
+}
