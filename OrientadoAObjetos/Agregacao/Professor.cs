269b9e7f2711b9a00238426c1706e37c58e7e63a
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-03-09 11:16:13 -0300

    docs: Agregação

---
+++ OrientadoAObjetos/Agregacao/Professor.cs
@@ -0,0 +1,10 @@
+class Professor
+{
+    public string Nome { get; set; }
+    public string Disciplina { get; set; }
+    public Professor(string nome, string disciplina)
+    {
+        Nome = nome;
+        Disciplina = disciplina;
+    }
+}
---
+++ OrientadoAObjetos/Agregacao/Program.cs
@@ -0,0 +1,23 @@
+﻿Professor professor1 = new Professor("Maicon", "Frontend");
+Professor professor2 = new Professor("Mirela", "Backend");
+Professor professor3 = new Professor("Gabriel", "Infraestrutura");
+Professor professor4 = new Professor("Matheus", "Economia");
+Professor professor5 = new Professor("Naruto", "Gestão empresarial");
+Professor professor6 = new Professor("Yuta", "Gestão ambiental");
+
+Departamento departamento1 = new Departamento("Computação");
+departamento1.IncluirProfessor(professor1);
+departamento1.IncluirProfessor(professor2);
+departamento1.IncluirProfessor(professor3);
+
+departamento1.ListaProfessores();
+
+
+Departamento departamento2 = new Departamento("Negócios");
+departamento2.IncluirProfessor(professor4);
+departamento2.IncluirProfessor(professor5);
+departamento2.IncluirProfessor(professor6);
+
+departamento2.ListaProfessores();
+
+Console.ReadKey();
