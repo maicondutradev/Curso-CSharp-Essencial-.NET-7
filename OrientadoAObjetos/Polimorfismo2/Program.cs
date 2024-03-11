f70e775e09671489dc10a769e611767b667c9c99
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-03-11 09:27:48 -0300

    docs: Polimorfismo 2

---
+++ OrientadoAObjetos/Polimorfismo2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Calcular cal = new Calcular();
+
+Console.WriteLine(cal.Somar(30, 40));
+
+Console.WriteLine(cal.Somar(30, 40, 50));
+
+class Calcular
+{
+    public int Somar(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    public int Somar(int num1, int num2, int num3)
+    {
+        return num1 + num2 + num3;
+    }
+}
