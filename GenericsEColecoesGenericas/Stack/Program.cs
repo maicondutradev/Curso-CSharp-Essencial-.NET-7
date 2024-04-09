86622ab31636709c65b20554531840c1eefb0382
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-09 09:25:27 -0300

    docs: Stack

---
+++ GenericsEColecoesGenericas/Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿//O ultimo a entrar é o primeiro a sair
+Stack<int> numeros = new Stack<int>();
+numeros.Push(1);
+numeros.Push(2);
+numeros.Push(2);
+numeros.Push(4);
+numeros.Push(5);
+
+
+Console.WriteLine($"A pilha original contém {numeros.Count} itens");
+
+ExibirPilha(numeros);
+
+Console.WriteLine($"\nItem obtido da pilha (Peek) : {numeros.Peek()}");
+
+Console.WriteLine($"\nItem removido da pilha (Pop) : {numeros.Pop()}");
+
+ExibirPilha(numeros);
+
+Console.WriteLine($"");
+
+static void ExibirPilha<T>(IEnumerable<T> numeros)
+{
+    Console.WriteLine();
+    foreach (var numero in numeros)
+    {
+        Console.WriteLine(numero);
+    }
+}
