2a4a7a361f19c4c1218863e0b68b743a092ff81b
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-09 10:50:45 -0300

    docs: Queue

---
+++ GenericsEColecoesGenericas/Queue/Program.cs
@@ -0,0 +1,21 @@
+﻿//O primeiro a entrar é o primeiro a sair
+Queue<string> diasSemana = new Queue<string>();
+
+diasSemana.Enqueue("Segunda-Feira");
+diasSemana.Enqueue("Terça-Feira");
+diasSemana.Enqueue("Quarta-Feira");
+
+ExibirFila(diasSemana);
+
+Console.WriteLine("\nMétodo Dequeue - Remove item do início da fila");
+diasSemana.Dequeue();
+
+ExibirFila(diasSemana);
+
+static void ExibirFila(Queue<string> diasSemana)
+{
+    foreach (var item in diasSemana)
+    {
+        Console.WriteLine(item);
+    }
+}
