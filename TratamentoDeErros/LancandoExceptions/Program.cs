ab921c128872226bb8dc5fdaae1d05076eca4474
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-10 09:27:51 -0300

    docs: Throw

---
+++ TratamentoDeErros/LancandoExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+try
+{
+    A.ProcessarA();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("\nTratando o erro em Main\n");
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
+class A
+{
+    public static void ProcessarA()
+    {
+        B.ProcessarB();
+    }
+}
+class B
+{
+    public static void ProcessarB()
+    {
+        C.ProcessarC();
+    }
+}
+class C
+{
+    public static void ProcessarC()
+    {
+        throw new NotImplementedException("Método não implementado");
+    }
+}
+
+
