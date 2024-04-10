efc9f6304a50036bc84324e10daa82bf2dda87c8
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-10 09:42:02 -0300

    docs: Filtro de exceções

---
+++ TratamentoDeErros/FiltroExcecoes/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.WriteLine("\nInforme o dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nInforme o divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int resultado = dividendo / divisor;
+    Console.WriteLine($"\n{dividendo} / {divisor} = {resultado}\n");
+}
+catch (DivideByZeroException) when (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+{
+    Console.WriteLine("\nHoje é sexta-feira e não capturamos estas exceções por motivos óbvios... \n \n Sextouuuuuu\n\n");
+}
+catch (Exception ex) when (ex.Message.Contains("format"))
+{
+    Console.WriteLine("Filtro de exceção com format");
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
