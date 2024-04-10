c958bc66d6bafcc49fc1bc1e01e3583baf4e4334
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-10 08:59:21 -0300

    docs: Try-Catch

--- TratamentoDeErros/Try-Catch-Finally/Program.cs
+++ TratamentoDeErros/Try-Catch-Finally/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    Console.WriteLine("\nInforme o dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nInforme o divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int resultado = dividendo / divisor;
+    Console.WriteLine($"\n{dividendo} / {divisor} = {resultado}\n");
+}
+catch (FormatException)
+{
+    Console.WriteLine($"\n Informe um valor inteiro.");
+}
+catch (OverflowException)
+{
+    Console.WriteLine($"\n Informe um valor inteiro entre 1 e 999999.");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine($"\n Não existe divisão por 0.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
