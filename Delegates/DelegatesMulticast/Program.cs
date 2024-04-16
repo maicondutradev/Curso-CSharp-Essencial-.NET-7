37698392e6c3f92bd2c3552bf5eee0c6ea291df3
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-16 09:25:00 -0300

    Csharp

---
+++ Delegates/DelegatesMulticast/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        MeuDelegate delegateMulticast = new MeuDelegate(Metodo1);
+        delegateMulticast += Metodo2;
+
+        delegateMulticast.Invoke("Olá, multicast delegate!");
+    }
+
+    static void Metodo1(string mensagem)
+    {
+        Console.WriteLine("Método 1: " + mensagem);
+    }
+    static void Metodo2(string mensagem)
+    {
+        Console.WriteLine("Método 2: " + mensagem);
+    }
+
+    public delegate void MeuDelegate(string mensagem);
+}
---
+++ Delegates/FuncoesAnonimas/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Imprimir imprimir = delegate (int valor)
+        {
+            Console.WriteLine($"Método anônimo. Valor: {valor}");
+        };
+
+        imprimir(100);
+    }
+
+    public delegate void Imprimir(int valor);
+}
