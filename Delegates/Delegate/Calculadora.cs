924633fff95c2802f4a67e233f4999474e051d1c
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-04-16 09:06:55 -0300

    docs: Delegates

---
+++ Delegates/Delegate/Calculadora.cs
@@ -0,0 +1,19 @@
+public class Calculadora
+{
+    public static float Somar(float n1, float n2)
+    {
+        return n1 + n2;
+    }
+    public static float Multiplicar(float n1, float n2)
+    {
+        return n1 * n2;
+    }
+    public static float Subtrair(float n1, float n2)
+    {
+        return n1 - n2;
+    }
+    public static float Dividir(float n1, float n2)
+    {
+        return n1 / n2;
+    }
+}
---
+++ Delegates/Delegate/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var resultado = Calculadora.Somar(5, 5);
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
+
+DelegateCalculadora calc = new DelegateCalculadora(Calculadora.Somar);
+var res = calc.Invoke(20, 30);
+Console.WriteLine(res);
+
+
+public delegate float DelegateCalculadora(float x, float y);
